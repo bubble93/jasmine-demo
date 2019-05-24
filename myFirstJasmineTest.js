c27b283b608c4905d26006df22aa7c9ef666198d
bubble <17791205330@163.com>
2019-05-24 18:20:16 +0800

    1

--- myFirstJasmineTest.js
+++ myFirstJasmineTest.js
@@ -1,16 +1,17 @@
-describe("My first Jasmine test", function() {
-  it("a spec with an expectation", function() {
-    expect(1).toBe(2);
-    expect(1===1).toBe(true);
-    expect('a').not.toBe('b');
-  });
-  
-  it("an other spec in current suite", function() {
-      expect(true).toBe(true);
-  });
-});
+describe("A suite of basic functions", function () {
+  var name;
 
-describe("My first Jasmine test", function() {
-  it("nothing", function() {
+  it("sayHello", function () {
+    name = "Conan";
+    var exp = "Hello Conan";
+    expect(exp).toEqual(sayHello(name));
+  });
+  it("reverse word", function () {
+    expect("DCBA").toEqual(reverse("ABCD"));
+    expect("Conan").toEqual(reverse("nanoC"));
+  });
+  it("toBe and not.toBe", function () {
+    expect(2).toBe(2);
+    expect('a').not.toBe('b');
   });
 });
